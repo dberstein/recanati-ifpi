457675728fd4e0c90b8faa179b73eb308a56ed75
Daniel Berstein <daniel@basegeo.com>
2024-11-28 06:59:06 +0200

    tweaks

--- src/html/index.php
+++ src/html/index.php
@@ -32,9 +32,11 @@ class Item
                 $this->album
             );
         } else {
+            $green = '#9de59d';
+            $red = '#d77d7d';
             return sprintf(
-                '<tr style="%s"><td>%s</td><td>%s</td><td>%s</td></tr>',
-                'background-color: ' . ($this->allowed ? '#9de59d' : '#d77d7d'),
+                "<tr style='%s'><td>%s</td><td>%s</td><td>%s</td></tr>\n",
+                'background-color: ' . ($this->allowed ? $green : $red),
                 htmlentities($this->artist),
                 htmlentities($this->song),
                 htmlentities($this->album),
@@ -51,59 +53,100 @@ $album = (string) @$_GET['album'];
 <html>
 
 <head>
-
+    <style>
+        table.results {
+            border-collapse: collapse;
+            border: 1px solid black;
+            width: 80%;
+            margin-left: 10%;
+        }
+        td {
+            border: 1px solid black;
+        }
+    </style>
 </head>
 
 <body>
     <form>
-        <label for="artist">Artist</label>
-        <input name="artist" value="<?= @htmlentities($artist) ?>" />
-        <label for="song">Song</label>
-        <input name="song" value="<?= @htmlentities($song) ?>" />
-        <label for="album">Album</label>
-        <input name="album" value="<?= @htmlentities($album) ?>" />
-
-        <input type="submit" />
+        <fieldset>
+            <legend>Search artist/song/album</legend>
+            <table>
+                <tr>
+                    <td>
+                        <label for="artist">Artist</label>
+                    </td>
+                    <td>
+                        <input name="artist" style="width: 500px;" value="<?= @htmlentities($artist) ?>" /><br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="song">Song</label>
+                    </td>
+                    <td>
+                        <input name="song" style="width: 500px;" value="<?= @htmlentities($song) ?>" /><br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="album">Album</label>
+                    </td>
+                    <td>
+                        <input name="album" style="width: 500px;" value="<?= @htmlentities($album) ?>" /><br/>
+                    </td>
+                </tr>
+            </table>
+            <input type="submit" />
+        </fieldset>
     </form>
-    <?php
-    if (!empty($artist . $song . $album)) {
-        $ifpi = new Ifpi($artist, $song, $album);
 
-        $dom = $ifpi->fetch(1);
-        $xpath = new DOMXPath($dom);
-        ?>
-        <table>
+<?php
+    if (!empty($artist . $song . $album)) {
+?>
+        <table class="results">
             <tr>
                 <th>Artist</th>
                 <th>Song</th>
                 <th>Album</th>
             </tr>
-            <?php
+<?php
+            $ifpi = new Ifpi($artist, $song, $album);
+            $page = 0;
+            while (true) {
+                $page++;
 
-            $i = 0;
-            foreach ($xpath->query("//*[@id]") as $elem) {
-                if (!preg_match('/^c[0-9]+/', $elem->id)) {
-                    continue;
+                $dom = $ifpi->fetch($page);
+                $xpath = new DOMXPath($dom);
+                $i = 0;
+                foreach ($xpath->query("//*[@id]") as $elem) {
+                    if (!preg_match('/^c[0-9]+/', $elem->id)) {
+                        continue;
+                    }
+
+                    $i++;
+                    $lines = array_filter(array_map(function ($s) {
+                        return trim($s);
+                    }, explode("\n", $elem->textContent)));
+
+                    echo new Item(
+                        $ifpi->allowed($xpath, $i),
+                        trim($lines[1]),
+                        trim($lines[2]),
+                        trim($lines[4]),
+                    );
                 }
 
-                $i++;
-                $lines = array_filter(array_map(function ($s) {
-                    return trim($s);
-                }, explode("\n", $elem->textContent)));
-
-                echo new Item(
-                    $ifpi->allowed($xpath, $i),
-                    trim($lines[1]),
-                    trim($lines[2]),
-                    trim($lines[4]),
-                );
+                if (0 == count($xpath->query('/html/body/div[2]/div/div/div[4]/div[58]/div[2]/img'))) {
+                    break;
+                }
             }
-            ?>
+
+?>
 
         </table>
-        <?php
+<?php
     }
-    ?>
-</body>
+?>
+    </body>
 
 </html>
