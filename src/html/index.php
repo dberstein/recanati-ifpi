bcd6e25bffdc5a6e09266102f237879b267c6a5e
Daniel Berstein <daniel@basegeo.com>
2024-12-04 18:09:23 +0200

    disable submit until file chosen

--- src/html/index.php
+++ src/html/index.php
@@ -15,7 +15,18 @@ $album = (string) @$_REQUEST['album'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.getElementById('fileToUpload');
+    const submitButton = document.getElementById('upload-btn');
 
+    submitButton.disabled = true;
+
+    fileInput.addEventListener('change', () => {
+        submitButton.disabled = fileInput.files.length === 0;
+    });
+});
+</script>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
